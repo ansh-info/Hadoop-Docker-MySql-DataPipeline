681e9d08322961b873961fd6e26e0abf7a19768d
ansh-info <anshkumar.info@gmail.com>
2024-07-12 19:29:58 +0200

    deleted files

--- hadoop_spark-cluster/config/spark-env.sh
+++
@@ -1,2 +0,0 @@
-export HADOOP_CONF_DIR="${HADOOP_HOME}/etc/hadoop"
-export SPARK_DIST_CLASSPATH=$($HADOOP_HOME/bin/hadoop classpath)
--- hadoop_spark-cluster/delete-volumes.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-sudo docker volume rm hdfs_master_data
-sudo docker volume rm hdfs_master_index
-sudo docker volume rm hdfs_master_checkpoint
-sudo docker volume rm hdfs_worker_data
